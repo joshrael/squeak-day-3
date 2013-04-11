490c03ccf23000e9ea154211fa5f0389c1d0c97c
Josh Rael <joshuarael@gmail.com>
2013-04-10 20:40:57 -0700

    added yelp api functionality

--- app.py
+++ app.py
@@ -7,11 +7,21 @@ from flask import (
     url_for
 )
 
+import urllib
+import requests
+import json
+from oauth import sign_url
+
 app = Flask(__name__)
+
+YELP_SEARCH_URL = 'http://api.yelp.com/v2/search'
 
 
 @app.route('/')
 def index():
+    # url args:
+    # http://localhost:5000/?data1=5&data2=7
+    # print request.args['data1']
     return render_template('index.html')
 
 
@@ -19,11 +29,40 @@ def index():
 def results():
     search_term = request.form['term']
     location = request.form['location']
-    numbers = range(10)
+
+    # will print search term in terminal
+    # print "search: %s" % search_term
+
+
+    # term and location are keys for the yelp api
+    # they just happen to be the same as what we had
+    # in the html file
+    data = {
+    'term': search_term,
+    'location': location
+    }
+
+    query_string = urllib.urlencode(data) # encode this data
+
+    # put the search terms in the url
+    api_url = YELP_SEARCH_URL + "?" + query_string
+
+    # authenticate the url with the yelp api
+    signed_url = sign_url(api_url)
+
+    # get the desired results from yelp
+    # requests is a library
+    # returns in the JSON format
+    response = requests.get(signed_url)
+
+    # get the fields from the json document:
+    json_response = json.loads(response.text)
+
     return render_template('results.html',
                             search_term=search_term,
                             location=location,
-                            numbers=numbers)
+                            businesses=json_response['businesses']) 
+                            # the businesses array in the json document
 
 if __name__ == '__main__':
     app.run(debug=True)
